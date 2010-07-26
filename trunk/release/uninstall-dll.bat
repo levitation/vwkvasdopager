64f83be429ecca91f3ab30a48251197010d79a64
kvasdopil <kvasdopil@c78c3a14-a42f-11dd-988f-d3ef9af6d102>
2010-07-26 07:05:12 +0000

    issue 29 fixed - install-dll.bat doesn't work when selecting "Run as Administrator" from the explorer context menu
    thanks to alexandre

--- trunk/release/uninstall-dll.bat
+++ trunk/release/uninstall-dll.bat
@@ -1,11 +1,11 @@
 @echo off
 if %PROCESSOR_ARCHITECTURE% == X86 (
-  regsvr32 /u vwKvasdoPagerBandwin32.dll
+  regsvr32 /u "%~dp0\vwKvasdoPagerBandwin32.dll"
 ) else (
   if %PROCESSOR_ARCHITECTURE% == x86 (
-    regsvr32 /u vwKvasdoPagerBandwin32.dll
+    regsvr32 /u "%~dp0\vwKvasdoPagerBandwin32.dll"
   ) else (
-    regsvr32 /u vwKvasdoPagerBandx64.dll
+    regsvr32 /u "%~dp0\vwKvasdoPagerBandx64.dll"
   )
 )
 
